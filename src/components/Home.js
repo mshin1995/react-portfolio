f0d53d8c980c786eb4d6f493872ddde9313a8b14
Matthew <mshin1995@gmail.com>
2020-02-11 18:25:51 -0800

    adding bootstrap

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="top-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2 text">
+                        <h1>MATTHEW SHIN</h1>
+                        <div className="description">
+                            <p>Software Developer | Seattle, WA</p>
+                        </div>
+                        <div className="top-big-link">
+                            <a className="btn btn-primary btn-link-1" href="resume/Resume.pdf" target="_blank"><i className="far fa-file-alt"></i> Resume</a>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    );
+};
+
+export default Home
