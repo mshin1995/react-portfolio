8635ec75c3e60e834b31fd87b29e04089332d35c
Matthew <mshin1995@gmail.com>
2020-02-12 17:42:19 -0800

    technology

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Nav from './components/Nav';
+import Technology from './components/Technology';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Home />
       <About />
+      <Technology />
     </div>
   );
 };
---
+++ src/components/Technology.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+const Technology = () => {
+    return (
+        <div className="technology-container section-container section-container-gray-bg">
+	        <div className="container">
+	            <div className="row">
+	                <div className="col technology section-description">
+	                    <h2>Technologies</h2>
+	                    <div className="divider-1"><span></span></div>
+	                </div>
+				</div>
+				<br />
+	            <div className="row">
+                	<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/javascript.png" alt="js" />
+							<br />
+							<span>JavaScript</span>
+						</div>
+                    </div>
+                    <div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/html.png" alt="html" />
+							<br />
+							<span>HTML5</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/css.png" alt="css" />
+							<br />
+							<span>CSS3</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/react.png" alt="react" />
+							<br />
+							<span>React</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/ruby.png" alt="ruby" />
+							<br />
+							<span>Ruby</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/rails.png" alt="rails" />
+							<br />
+							<span>Rails</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/postgresql.png" alt="postgresql" />
+							<br />
+							<span>PostgreSQL</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/git.png" alt="git" />
+							<br />
+							<span>Git</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/bootstrap.png" alt="bootstrap" />
+							<br />
+							<span>Bootstrap</span>
+						</div>
+					</div>
+					<div className="col-md-3 technology-wrapper">
+						<div>
+							<img src="icons/semantic.png" alt="semantic" />
+							<br />
+							<span>Semantic UI</span>
+						</div>
+					</div>
+				</div>
+	        </div>
+		</div>
+    );
+};
+
+export default Technology
