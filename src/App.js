7dc0a307ca651c455f2a9c18c63146fcdd673414
Matthew <mshin1995@gmail.com>
2020-02-18 20:08:43 -0800

    contact section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
-import Nav from './components/Nav';
 import Projects from './components/Projects';
 import Technology from './components/Technology';
+import Contact from './components/Contact'
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Projects />
       <Technology />
+      <Contact />
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,20 @@ import React from 'react';
 const Contact = () => {
     return (
         <footer>
-	        <div class="container footer-container">
-	        	<div class="row">
-                    <div class="col-md-5 footer-left">
+	        <div className="container footer-container">
+	        	<div className="row">
+                    <div className="col-md-5 footer-left">
 						<p>Email: <a href="mailto:mshin1995@gmail.com" target="_blank">mshin1995@gmail.com</a></p>
                     </div>
                     
-                    <div class="col-md-6 offset-md-1 footer-right">
-                    	<a href="https://www.linkedin.com/in/mshin1995" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-						<a href="https://www.github.com/mshin1995" target="_blank"><i class="fab fa-github"></i></a>
+                    <div className="col-md-6 offset-md-1 footer-right">
+                    	<a href="https://www.linkedin.com/in/mshin1995" target="_blank"><i className="fab fa-linkedin-in"></i></a>
+						<a href="https://www.github.com/mshin1995" target="_blank"><i className="fab fa-github"></i></a>
                     </div>
                 </div>
-                <div class="row">
-                	<div class="col footer-bottom">
-		           		<a class="scroll-link" href="#top-content"><i class="fas fa-chevron-up"></i></a>
+                <div className="row">
+                	<div className="col footer-bottom">
+		           		<a className="scroll-link" href="#top-content"><i className="fas fa-chevron-up"></i></a>
 		           	</div>
                 </div>
 	        </div>
